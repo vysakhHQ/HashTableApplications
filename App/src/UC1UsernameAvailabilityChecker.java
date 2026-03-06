87532fa6bb6ec54917c70d703ea390908a6b8f59
VysakhHQ <vysakhmethalayil@gmail.com>
2026-03-06 09:47:00 +0530

    UC1: Username Availability Checker

---
+++ App/src/UC1UsernameAvailabilityChecker.java
@@ -0,0 +1,18 @@
+import java.util.HashMap;
+import java.util.Scanner;
+public class Problem1UsernameAvailabilityChecker{
+    public static void main(String[] args){
+        HashMap<String,Integer> usernames=new HashMap<>();
+        usernames.put("john_doe",1);
+        usernames.put("alice",2);
+        usernames.put("admin",3);
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter username:");
+        String username=sc.nextLine();
+        if(usernames.containsKey(username)){
+            System.out.println("Username already taken");
+        }else{
+            System.out.println("Username available");
+        }
+    }
+}
